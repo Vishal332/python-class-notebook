85148a6d12e7e96fe4a297f2ce2af750166fffc2
Vishal332 <vishalshaki7790@gmail.com>
2025-09-22 10:40:40 +0530

    test

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # python-class-notebook
+# this is main
+## this is main test branch
+
+
---
+++ intro.py
@@ -0,0 +1,9 @@
+a=2;
+b=8;
+c=a+b;
+print(c);
+print(type(c));
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+c = a + b
+print("The sum of", a, "and", b, "is", c)
